17452b33223f25f60c1ec968bb88965e775ae405
Tom Counsell <other@tomcounsell.com>
2020-06-02 22:38:06 +0700

    shop menu items are Publishable Expirable and Annotatable

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from apps.shop.models import Shop
+from apps.shop.models import Shop, Menu, Item
 
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
     pass
---
+++ apps/shop/models/item.py
@@ -0,0 +1,40 @@
+from django.db import models
+from simple_history.models import HistoricalRecords
+from djmoney.models.fields import MoneyField
+from django.contrib.postgres.fields import ArrayField
+from apps.common.behaviors import Timestampable, Publishable, Expirable, Annotatable
+from apps.shop.models.menu import PERIOD_CHOICES
+
+
+class Item(Timestampable, Publishable, Expirable, Annotatable, models.Model):
+
+    name = models.CharField(max_length=140, blank=False)
+    description = models.TextField(default='', blank=True)
+    image = models.OneToOneField('common.Image', null=True, on_delete=models.SET_NULL, related_name='item')
+    alt_images = models.ManyToManyField('common.Image')
+
+    menu = models.ForeignKey('shop.Menu', null=False,
+                             on_delete=models.PROTECT, related_name="items")
+    menu_section = models.ForeignKey('shop.MenuSection', null=True, blank=True,
+                                     on_delete=models.SET_NULL, related_name='items')
+    # alt_menu_sections = models.ManyToManyField('shop.MenuSection', related_name='alt_items')
+
+    # allergies = models.ManyToManyField('common.FoodAllergy')
+    # feature_ingredients = models.ManyToManyField('common.FoodIngredient')
+    # kitchen_goods = models.ManyToManyField('production.Good')
+
+    price = MoneyField(max_digits=8, decimal_places=2, null=True, default_currency='THB')
+    # promotion_price - Promotion model has item, promo_price, Publishable, Expirable
+
+    unavailable_periods = ArrayField(
+        models.CharField(choices=PERIOD_CHOICES, max_length=4), default=list
+    )
+
+    # HISTORY MANAGER
+    history = HistoricalRecords()
+
+    # MODEL PROPERTIES
+
+    # MODEL FUNCTIONS
+    def __str__(self):
+        return f"{self.shop.name} Menu"
