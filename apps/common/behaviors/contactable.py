a0a2b2381847af15d4ac921e951ad538531810f7
Tom Counsell <other@tomcounsell.com>
2020-06-02 22:33:54 +0700

    make contactable model more intuitive as a mixin

--- apps/common/behaviors/contactable.py
+++ apps/common/behaviors/contactable.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class Contactable(models.Model):
-    primary_phone_number = models.CharField(max_length=15, null=True, blank=True)
-    secondary_phone_number = models.CharField(max_length=15, null=True, blank=True)
-    contact_email = models.EmailField(null=True, blank=True)
-    sales_email = models.EmailField(null=True, blank=True)
+    contact_name = models.CharField(max_length=50, blank=True, help_text="name of point of contact")
+    contact_phone_number = models.CharField(max_length=15, blank=True, help_text='public contact phone')
+    contact_email = models.EmailField(blank=True, help_text='public contact email')
 
     class Meta:
         abstract = True
