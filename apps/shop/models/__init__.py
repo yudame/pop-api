9d595eb829f9c2c5dd93f3c99d4a324700549a1e
Tom Counsell <other@tomcounsell.com>
2020-06-05 13:19:52 +0700

    move addons to groups

--- apps/shop/models/__init__.py
+++ apps/shop/models/__init__.py
@@ -1,6 +1,11 @@
 from apps.shop.models.shop import Shop
 from apps.shop.models.menu import Menu, MenuCategory, MenuSection
 from apps.shop.models.item import Item
-from apps.shop.models.addon import AddonType, Addon
+from apps.shop.models.addon import ItemAddonGroup, AddonGroup, Addon
 
-__all__ = ['Shop', 'Menu', 'MenuCategory', 'MenuSection', 'Item', 'AddonType', 'Addon', ]
+__all__ = [
+    'Shop',
+    'Menu', 'MenuCategory', 'MenuSection',
+    'Item',
+    'ItemAddonGroup', 'AddonGroup', 'Addon',
+]
