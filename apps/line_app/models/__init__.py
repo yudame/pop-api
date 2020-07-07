035ba8bb04c167e97378ab30f99667ae21f52246
Tom Counsell <other@tomcounsell.com>
2020-07-07 19:02:55 +0700

    migration for line rich menu model

--- apps/line_app/models/__init__.py
+++ apps/line_app/models/__init__.py
@@ -1,6 +1,11 @@
 from apps.line_app.models.line_channel import LineChannel
 from apps.line_app.models.line_user_profile import LineUserProfile
 from apps.line_app.models.line_channel_membership import LineChannelMembership
-# from apps.line_app.models.line_rich_menu import LineRichMenu
+from apps.line_app.models.line_rich_menu import LineRichMenu
 
-__all__ = ['LineChannel', 'LineUserProfile', 'LineChannelMembership',]
+__all__ = [
+    'LineChannel',
+    'LineUserProfile',
+    'LineChannelMembership',
+    'LineRichMenu',
+]
