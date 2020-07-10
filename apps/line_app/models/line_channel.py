ccf9354cbcbb48f002405a4e11f6bf64e915f019
Tom Counsell <other@tomcounsell.com>
2020-07-10 12:11:13 +0700

    add help text

--- apps/line_app/models/line_channel.py
+++ apps/line_app/models/line_channel.py
@@ -18,8 +18,10 @@ class LineChannel(Timestampable, models.Model):
     privacy_policy_url = models.URLField(null=True, blank=True)
     terms_url = models.URLField(null=True, blank=True)
 
-    numeric_id = models.CharField(max_length=10, null=True, blank=True)
-    secret = models.CharField(max_length=32, null=True, blank=True)
+    numeric_id = models.CharField(max_length=10, null=True, blank=True,
+                                  help_text="Channel ID at top of basic settings screen")
+    secret = models.CharField(max_length=32, null=True, blank=True,
+                              help_text="Channel secret on basic settings screen")
     assertion_signing_key = models.CharField(max_length=40, null=True, blank=True)
     bot_id = models.CharField(max_length=31, null=True, blank=True)
     access_token = models.CharField(max_length=200, null=True, blank=True)
@@ -46,7 +48,8 @@ class LineChannel(Timestampable, models.Model):
                 return 'menu coming soon'
                 # return MenuMessage
             elif line_event.message.text == "delivery":
-                return Delivery.render_bot_message()
+                delivery = Delivery()
+                return delivery.render_bot_message()
             return line_event.message.text
         elif isinstance(line_event.message, LocationMessage):
             line_user_profile = LineUserProfile.objects.get(line_user_id=line_event.source.user_id)
