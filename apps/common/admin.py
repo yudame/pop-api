f13fe897a2e973f9404e670f09ef34f500d92ee0
Tom Counsell <other@tomcounsell.com>
2020-07-12 19:40:34 +0700

    add some common models to admin

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from apps.common.models import Image, Address, Note
+
 
 class CSSAdminMixin(object):
     class Media:
@@ -9,4 +11,18 @@ class CSSAdminMixin(object):
 
 
 class MyModelAdmin(admin.ModelAdmin, CSSAdminMixin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(MyModelAdmin):
+    pass
+
+@admin.register(Address)
+class AddressAdmin(MyModelAdmin):
+    pass
+
+
+@admin.register(Note)
+class NoteAdmin(MyModelAdmin):
     pass
