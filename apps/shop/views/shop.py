bc8be27bd9147325731f2c42e5e0844bfa312d06
Tom Counsell <other@tomcounsell.com>
2020-07-15 23:41:33 +0700

    kickass model forms for shop setup

--- apps/shop/views/shop.py
+++ apps/shop/views/shop.py
@@ -1,55 +1,10 @@
-from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms import ModelForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
-from django.utils.translation import gettext_lazy as _
-from fontawesome_5 import Icon
 
+from apps.line_app.models import LineChannel
 from apps.shop.models import Shop
-
-
-class BootstrapModelForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        icons = getattr(self.Meta, 'icons', dict())
-        for field_name, field in self.fields.items():
-            # add form-control class to all fields
-            field.widget.attrs['class'] = 'form-control'
-            # set default textarea size to just 4 rows
-            if isinstance(field.widget, forms.Textarea):
-                field.widget.attrs['rows'] = 4
-            # set icon attr on field object
-            if field_name in icons:
-                field.icon = icons[field_name]
-
-
-class ShopFormA(BootstrapModelForm):
-    class Meta:
-        model = Shop
-        fields = [
-            'name', 'description', 'unstructured_text_address',
-        ]
-        labels = {
-            'name': _('Name'), 'description': _('About'), 'unstructured_text_address': _('Full Address')
-        }
-        icons = {
-            'name': Icon('store'),
-        }
-        required_fields = ['name',]
-
-class ShopFormB(BootstrapModelForm):
-    class Meta:
-        model = Shop
-        fields = [
-            'facebook_href', 'instagram_href', 'google_maps_href',
-        ]
-        labels = {
-            'facebook_href': _('Facebook Profile Link'), 'instagram_href': _('Instagram Profile Link'), 'google_maps_href': _('Google Maps Profile Link')
-        }
-        icons = {
-            'facebook_href': Icon('facebook', 'fab'), 'instagram_href': Icon('instagram', 'fab'), 'google_maps_href': Icon('map-marker'),
-        }
+from apps.shop.views.shop_setup_forms import ShopFormA, ShopFormB, LineChannelFormA, LineChannelFormB
 
 
 class SetupView(LoginRequiredMixin, View):
@@ -65,20 +20,27 @@ class SetupView(LoginRequiredMixin, View):
         if not request.session.get('shop_setup_form_index'):
             request.session['shop_setup_form_index'] = 0
 
-        self.shop_setup_forms = [ShopFormA, ShopFormB]
+        self.shop_setup_forms = [ShopFormA, ShopFormB, LineChannelFormA, LineChannelFormB]
         self.current_form = self.shop_setup_forms[request.session.get('shop_setup_form_index', 0)]
 
         return super().dispatch(request, *args, **kwargs)
 
+    def get_form_class_instance(self):
+        if self.current_form.Meta.model == Shop:
+            return self.shop
+        elif self.current_form.Meta.model == LineChannel:
+            line_channel, lc_created = LineChannel.objects.get_or_create(shop=self.shop)
+            return line_channel
+
     def get(self, request, *args, **kwargs):
         context = {
             'shop': self.shop,
-            'shop_setup_form': self.current_form(instance=self.shop)
+            'shop_setup_form': self.current_form(instance=self.get_form_class_instance())
         }
         return render(request, 'setup.html', context)
 
     def post(self, request, *args, **kwargs):
-        shop_setup_form = self.current_form(request.POST, instance=self.shop)
+        shop_setup_form = self.current_form(request.POST, instance=self.get_form_class_instance())
 
         if shop_setup_form.is_valid():
             shop_setup_form.save()
---
+++ apps/shop/views/shop_setup_forms.py
@@ -0,0 +1,106 @@
+
+from django import forms
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from fontawesome_5 import Icon
+
+from apps.line_app.models import LineChannel
+from apps.shop.models import Shop
+from settings import HOSTNAME
+
+
+class BootstrapModelForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        icons = getattr(self.Meta, 'icons', dict())
+        for field_name, field in self.fields.items():
+            # add form-control class to all fields
+            field.widget.attrs['class'] = 'form-control'
+            # set default textarea size to just 4 rows
+            if isinstance(field.widget, forms.Textarea):
+                field.widget.attrs['rows'] = 4
+            # set icon attr on field object
+            if field_name in icons:
+                field.icon = icons[field_name]
+
+
+class ShopFormA(BootstrapModelForm):
+    class Meta:
+        model = Shop
+        fields = [
+            'name', 'description', 'unstructured_text_address',
+        ]
+        labels = {
+            'name': _('Name'), 'description': _('About'), 'unstructured_text_address': _('Full Address')
+        }
+        icons = {
+            'name': Icon('store'),
+        }
+        required_fields = ['name',]
+
+class ShopFormB(BootstrapModelForm):
+    class Meta:
+        model = Shop
+        fields = [
+            'facebook_href', 'instagram_href', 'google_maps_href',
+        ]
+        labels = {
+            'facebook_href': _('Facebook Profile Link'), 'instagram_href': _('Instagram Profile Link'), 'google_maps_href': _('Google Maps Profile Link')
+        }
+        icons = {
+            'facebook_href': Icon('facebook', 'fab'), 'instagram_href': Icon('instagram', 'fab'), 'google_maps_href': Icon('map-marker'),
+        }
+
+class LineChannelFormA(BootstrapModelForm):
+    class Meta:
+        model = LineChannel
+        fields = [
+            'numeric_id', 'name', 'description', 'email_address', 'secret', 'assertion_signing_key', 'creator_user_id',
+        ]
+        labels = {
+            'numeric_id': _('Channel ID'),
+            'name': _('Channel name'),
+            'description': _('Channel description'),
+            'email_address': _('Email address'),
+            'secret': _('Channel secret'),
+            'assertion_signing_key': _('Assertion Signing Key'),
+            'creator_user_id': _('Your user ID'),
+        }
+
+class LineChannelFormB(BootstrapModelForm):
+
+    line_bot_callback_uri = forms.CharField(widget=forms.Textarea(attrs={'readonly': 'readonly'}))
+    use_webhook = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    allow_groups = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    auto_reply = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    greeting = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    access_token = forms.CharField(widget=forms.Textarea())
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance', None)
+        if instance:
+            kwargs['initial'] = {
+                'line_bot_callback_uri': f"https://{HOSTNAME}{instance.line_bot_callback_uri}",
+                'use_webhook': _("ON"),
+                'allow_groups': _("Disabled"),
+                'auto_reply': _("Disabled"),
+                'greeting': _("Disabled"),
+            }
+        return super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = LineChannel
+        fields = [
+            'bot_id', 'line_bot_callback_uri', 'use_webhook', 'allow_groups', 'auto_reply', 'greeting', 'access_token',
+        ]
+        labels = {
+            'bot_id': _('Bot basic ID'),
+            'line_bot_callback_uri': _('Webhook URL'),
+            'use_webhook': _('Use webhook'),
+            'allow_groups': _('Allow bot to join group chats'),
+            'auto_reply': _('Auto-reply messages'),
+            'greeting': _('Greeting messages'),
+            'access_token': _('Channel access token (long-lived)'),
+        }
+        readonly_fields = ('line_bot_callback_uri', 'use_webhook')
+
