1bb7e16f4b63ef502367f76dda20d4dc74bf1858
Tom Counsell <other@tomcounsell.com>
2020-07-31 11:27:08 +0700

    import telegram features for future use

---
+++ apps/communication/telegram/commands/start.py
@@ -0,0 +1,39 @@
+import logging
+
+from apps.communication.models import TelegramBotMembership
+from apps.communication.telegram.commands.decorator import telegram_command
+from telegram import Message
+
+from apps.user.models import User
+
+
+@telegram_command("start")
+def start(message: Message, context):
+
+    logging.debug(context.__dict__)
+    if len(context.args) == 1:
+        (username, four_digit_login_code) = context.args[0].split(":", 1)
+        user = User.objects.get(username=username)
+        if user.four_digit_login_code == four_digit_login_code:
+            tbm, tbm_created = TelegramBotMembership.objects.get_or_create(
+                user=user,
+                telegram_user_id=message.from_user.id
+            )
+            tbm.telegram_user_dict = str({k: v for k, v in message.from_user.__dict__.items() if not k.startswith('_')})
+            tbm.save()
+
+            return "\n".join([
+                "Welcome to Bemmerse Media Bot!",
+                "You are now connected to your Bemmerse account.",
+                "try /help for a list of commands and examples.",
+                "Also, you can just start uploading photos and videos now.",
+            ])
+
+    return "\n".join([
+        "Welcome to Bemmerse Media Bot!",
+        "In order to upload and manage your media files, please go to",
+        "https://app.bemmerse.com/dashboard/",
+        "and find the Telegram button connect your Bemmerse account",
+    ])
+
+start.help_text = "get started"
