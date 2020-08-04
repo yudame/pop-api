2d5d44053a20dc21ad08d3e53f314b5c61d00c27
Tom Counsell <other@tomcounsell.com>
2020-08-04 15:26:50 +0700

    shortcut method for adding notes

--- apps/common/behaviors/annotatable.py
+++ apps/common/behaviors/annotatable.py
@@ -8,5 +8,10 @@ class Annotatable(models.Model):
     def has_notes(self):
         return True if self.notes.count() else False
 
+    def add_note(self, text_string):
+        from apps.common.models import Note
+        new_note = Note.objects.create(text=text_string)
+        self.notes.add(new_note)
+
     class Meta:
         abstract = True
