765a1ddd0384302ffe1bbba5b32f70d2d09e052f
Tom Counsell <other@tomcounsell.com>
2020-09-19 15:06:02 +0700

    menu editor uses vue js

--- apps/shop/views/dashboard.py
+++ apps/shop/views/dashboard.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 from django.shortcuts import render
+
+from apps.api.serializers.item import fast_serialize_item
 from apps.shop.models import Shop
 from apps.shop.views.setup import ShopOwnerRequiredMixin
 from apps.shop.views.shop import ShopViewMixin
@@ -13,4 +15,5 @@ class DashboardView(LoginRequiredMixin, ShopViewMixin, ShopOwnerRequiredMixin, V
 
 
     def get(self, request, *args, **kwargs):
+        # self.context['serialized_menu'] = {'items': [fast_serialize_item(item) for item in self.shop.menu.items.all()] }
         return render(request, 'dashboard.html', self.context)
